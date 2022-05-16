b83b11476098fb2a80de1d2e2d579e6999782e50
gavin-shaw <gavin@gnarus.io>
2022-05-16 20:06:03 +0200

    feat: add more components

--- ekp_sdk/ui/components/__init__.py
+++ ekp_sdk/ui/components/__init__.py
@@ -1,2 +1,3 @@
-from ekp_sdk.ui.components.chart import Chart
+from ekp_sdk.ui.components.chart import *
+from ekp_sdk.ui.components.link import *
 from ekp_sdk.ui.components.others import *
--- ekp_sdk/ui/rpc/__init__.py
+++ ekp_sdk/ui/rpc/__init__.py
@@ -1,1 +1,2 @@
-from ekp_sdk.ui.rpc.commify import commify
+from ekp_sdk.ui.rpc.commify import *
+from ekp_sdk.ui.rpc.format_mask_address import *
