03da2d276e2ed6638da6cff260429c1a1c3b275a
gavin-shaw <gavin@gnarus.io>
2022-06-16 09:40:31 +0200

    version: 0.7.9

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 with open("README.md", "r", encoding="latin-1") as fh:
     long_description = fh.read()
 
-VERSION = '0.7.8'
+VERSION = '0.7.9'
 DESCRIPTION = 'Python SDK for frontend components usage'
 LONG_DESCRIPTION = 'A package that allows to use front-end components to simplify ' \
                    'building of web pages for ekp plugins'
